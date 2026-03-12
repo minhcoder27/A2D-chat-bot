0a7149b627c315852f477b3c4d3c3f471d7b3546
tminh_hk20 <95926643+minhcoder27@users.noreply.github.com>
2026-03-12 16:02:29 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -12,21 +12,21 @@ export default {
     const pathname = url.pathname;
 
     // --- CẤU HÌNH BẢO MẬT (ẨN KHỎI FRONTEND) ---
-    const GOOGLE_CLIENT_ID = "";
+    const GOOGLE_CLIENT_ID = "305607144766-rsp6v0i6b0n83u989ojpinptq5a9l21c.apps.googleusercontent.com";
     const WEBHOOKS = {
-      chat: "",
-      delete: "",
-      contact: "",
-      google: "",
-      login: "",
-      signup: "",
-      verify: "",
-      forgot_req: "",
-      forgot_res: "",
-      problem_status_residency: "",
-      problem_status_labor: "",
-      problem_status_tax: "",
-      problem_status_insurance: ""
+      chat: "https://n8n.a2dzespol.pl/webhook/d8451693-0079-4da7-b3a3-ec1561bd2da3",
+      delete: "https://n8n.a2dzespol.pl/webhook/7e75278a-2bc2-4832-b601-39085570f7aa",
+      contact: "https://n8n.a2dzespol.pl/webhook/366a0816-3157-4425-9bb5-5aa258c94161",
+      google: "https://n8n.a2dzespol.pl/webhook/1bb050cf-1320-400d-823b-1dd76ac4daae",
+      login: "https://n8n.a2dzespol.pl/webhook/3349af0f-ead6-4ba5-a535-f9c993b8fe19",
+      signup: "https://n8n.a2dzespol.pl/webhook/f8f58561-ed9e-4a49-b273-7776ba9073a3",
+      verify: "https://n8n.a2dzespol.pl/webhook/e05cd05b-f854-48ca-bc15-ca75ca32edf9",
+      forgot_req: "https://n8n.a2dzespol.pl/webhook/6695aa05-2a9c-4abb-8c9b-28188af236ee",
+      forgot_res: "https://n8n.a2dzespol.pl/webhook/431e1515-154b-4545-b60b-edebc54b47a8",
+      problem_status_residency: "https://n8n.a2dzespol.pl/webhook/79e7d4cc-2b54-49ff-9d80-aa0834cdcdbe",
+      problem_status_labor: "https://n8n.a2dzespol.pl/webhook/ef1d55c7-a4b5-41b3-a559-bb2bf127db52",
+      problem_status_tax: "https://n8n.a2dzespol.pl/webhook/a1998a42-a743-4965-993e-e0eb97b36244",
+      problem_status_insurance: "https://n8n.a2dzespol.pl/webhook/5a51ee16-917b-4717-9fcb-1804bf922602"
     };
 
     // Endpoint lấy ID Google
@@ -85,5 +85,3 @@ export default {
     });
   }
 };
-
-
